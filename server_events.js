928ada0a25b14cfb9936cfc8d92571310d90ad18
Santiago Munín <burning1@gmail.com>
2013-10-29 20:00:55 +0100

    Came back to one single file

--- server_events.js
+++ server_events.js
@@ -1,10 +1,8 @@
 module.exports = function(io) {
 
-  require('./server_logic.js')(io);
-
+   // SERVER INTERFACE
   io.sockets.on('connection', function(socket) {
     console.log('Client connected');
-
 
     // LOGIN FUNCTIONS
     socket.on('login', function(nickname, callback) {
@@ -32,6 +30,7 @@ module.exports = function(io) {
     });
   });
 
+  // COMMUNICATION FUNCTIONS
   var sendMessage = function(subject, nickname, message) {
     io.sockets.emit(subject, nickname, message);
   };
@@ -47,5 +46,110 @@ module.exports = function(io) {
         }
     }
     return false;
+  };
+
+
+  // SERVER LOGIC
+  var people = {};
+
+  login = function(nickname, callback) {
+    console.log('Trying to set nickname ' + nickname);
+    var nickAvailable = isNicknameAvailable(nickname);
+
+    if (nickAvailable) {
+      people[nickname] = "";
+      socket.nickname = nickname;
+      callback(undefined, getClients());
+    } else {
+      callback('username_not_available', getClients());
+    }
+
+    return nickAvailable;
+  };
+
+  log_user_out = function(nickname) {
+    console.log("Logging "+nickname+" out");
+    people[nickname] = undefined;
+  };
+
+  request_chat = function(source, target, callback) {
+    var status = check_chat_request();
+    var target_socket;
+    if (!status.valid) {
+      callback(status.err, false);
+    } else {
+      target_socket = findSocket(target);
+      target_socket.emit('request_from', source, function (ok) {
+        if (ok) {
+          console.log("Successful connection: " + source + " - " + target);
+          set_chat(source, target);
+          callback(undefined, true);
+        } else {
+          callback('Request rejected', false);
+        }
+      });
+    }
+  };
+
+  leave_chat = function(nickname) {
+    var other_nickname = people[nickname];
+    if (other_nickname && other_nickname !== "") {
+      var other_socket = findSocket(other_nickname);
+      other_socket.emit('user_left_chat');
+      console.log("Disconnection between " + nickname + " and " + other_nickname);
+      remove_chat(nickname);
+    }
+  };
+
+  var check_chat_request = function(source, target) {
+    if (!isNicknameAvailable(source)) {
+      return { valid: false, error: "We don't recognize your nick" };
+    }
+
+    if (!isNicknameAvailable(target)) {
+      return { valid: false, error: "We don't recognize the target's nick" };
+    }
+
+    if (source === target) {
+      return { valid: false, error: "You can\'t speak to yourself!" };
+    }
+    return { valid: true };
+  };
+
+  var set_chat = function(source, target) {
+    people[source] = target;
+    people[target] = source;
+  };
+
+  var remove_chat = function(source) {
+    if (people[source]) {
+      people[people[source]] = "";
+      people[source] = "";
+    }
+  };
+
+  var isNicknameAvailable = function(nickname) {
+    return people[nickname] === undefined;
+  };
+
+  var findSocket = function (nickname) {
+    var sockets = io.sockets.clients();
+    for (var s in sockets) {
+      if (sockets.hasOwnProperty(s) && sockets[s].nickname === nickname) {
+        return sockets[s];
+      }
+    }
+    return undefined;
+  };
+
+  var getClients = function() {
+    console.log("clients? --> "+ people);
+    result = [];
+    for (var person in people) {
+      if (people.hasOwnProperty(person) && people[person] === "") {
+        result.push(person);
+      }
+    }
+    return result;
   };
 };
